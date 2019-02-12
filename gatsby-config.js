f52d8c249966157a364be6d77c809e23f51e5962
Danijel <dan.baternik@gmail.com>
2019-02-12 13:39:20 +1000

    Added ga to docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,30 @@ const plugins = [
             include_in_development: true,
         }
     },
+    {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+            trackingId: "91424010-2",
+            // Puts tracking script in the head instead of the body
+            head: false,
+            // Setting this parameter is optional
+            // anonymize: true,
+            // Setting this parameter is also optional
+            respectDNT: true,
+            // Avoids sending pageview hits from custom paths
+            // exclude: ["/preview/**", "/do-not-track/me/too/"],
+            // Enables Google Optimize using your container Id
+            // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+            // Enables Google Optimize Experiment ID
+            // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+            // Set Variation ID. 0 for original 1,2,3....
+            // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+            // Any additional create only fields (optional)
+            sampleRate: 5,
+            siteSpeedSampleRate: 10,
+            // cookieDomain: "docs.vwa.la",
+        },
+    },
     /**
      *  Utility Plugins
      */
