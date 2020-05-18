17680a33c2ae84506f5043144b633bb57946ef5a
Danijel <dan.baternik@gmail.com>
2020-05-18 14:49:24 +1000

    SMA oauth link fix

--- gatsby/createPages.js
+++ gatsby/createPages.js
@@ -29,7 +29,7 @@ module.exports = async ({ graphql, actions }) => {
         fromPath: `/releases`,
         isPermanent: true,
         redirectInBrowser: true,
-        toPath: `/releases/2019-01-31/`,
+        toPath: `/releases/2020-05-30/`,
     })
 
     createRedirect({
