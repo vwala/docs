dfe8a517349425b7d8f36a719ff0d21a7a512586
Danijel <dan.baternik@gmail.com>
2020-08-18 12:03:44 +1000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bb4d57ecf2b0dffbc520.js"
+    "url": "webpack-runtime-07a058875fc3f0fc83a9.js"
   },
   {
     "url": "app-1df96f36a560fdd33dce.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a968ea338708d605bd2e1d85ce589546"
+    "revision": "161cd1c579683a233e52fe1e67394021"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
